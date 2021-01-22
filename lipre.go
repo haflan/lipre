e76843a741019a37a7af5c2034d836edb873d87c
Vetle Haflan <vetle@haflan.dev>
2021-01-22 23:17:58 +0100

    Initial commit with proof of concept

---
+++ lipre.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+const temporaryCorrectRoomCode = "tester"
+
+type Room struct {
+	code      string
+	presenter *websocket.Conn
+	// TODO:
+	viewers []*websocket.Conn
+}
+
+// TODO: Lock on this for concurrency?
+var rooms = make(map[string]*Room)
+
+func (room *Room) listen() {
+	for {
+		_, message, err := room.presenter.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			delete(rooms, room.code)
+			return
+		}
+		for _, viewerConn := range room.viewers {
+			if viewerConn == nil {
+				break
+			}
+			viewerConn.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
+
+// HTTP
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	htmlData, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Write(htmlData)
+	return
+}
+
+func presentHandler(w http.ResponseWriter, r *http.Request) {
+	roomCode := mux.Vars(r)["roomCode"]
+	/*if roomCode != temporaryCorrectRoomCode {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}*/
+	fmt.Println("Upgrading connection")
+	conn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	room := &Room{code: roomCode, presenter: conn}
+	rooms[roomCode] = room
+	go room.listen()
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	roomCode := mux.Vars(r)["roomCode"]
+	room := rooms[roomCode]
+	if room == nil {
+		return
+	}
+	conn, err := wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	room.viewers = append(rooms[roomCode].viewers, conn)
+}
+
+func main() {
+	fmt.Println("Server starting")
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/pres/{roomCode}", presentHandler)
+	router.HandleFunc("/view/{roomCode}", viewHandler)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", 8080), nil))
+}
